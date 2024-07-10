b57bd6b6206d500653b642395b61af70e09ade2c
ali8137 <124887925+ali8137@users.noreply.github.com>
2024-07-10 17:37:55 +0300

    install tailwindcss/aspect-ratio using 'npm install @tailwindcss/aspect-ratio' for the store navigation tailwindUI component. then adding aspect tailwindcss ratio plugin to tailwind.config.json file

--- Ecommerce-frontend/tailwind.config.js
+++ Ecommerce-frontend/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import aspectRatio from '@tailwindcss/aspect-ratio';
+// the above was added when using the store navigation provided by tailwindui
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    aspectRatio
+    // the above was added when using the store navigation provided by tailwindui
+  ],
 }
