29c96297547122cfecbff81356d807e651c7e99d
ali8137 <124887925+ali8137@users.noreply.github.com>
2024-09-17 21:29:49 +0300

    some fixtion related to react-router to prepare for using it inside the frontend

--- Ecommerce-frontend/src/App.jsx
+++ Ecommerce-frontend/src/App.jsx
@@ -24,3 +24,53 @@ function App() {
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import {
+//   BrowserRouter,
+//   createBrowserRouter,
+//   createRoutesFromElements,
+//   Route,
+//   RouterProvider,
+// } from 'react-router-dom'
+// import './App.css'
+// // import Navigation from './customer/components/navigation/Navigation.jsx'
+// import HorizontalLinearStepper from './customer/components/checkout/HorizontalLinearStepper'
+// import HomePage from './pages/HomePage'
+// import { action } from './customer/components/checkout/DeliveryAddressForm'
+// // import HomePage from './pages/HomePage.jsx'
+// // import Product from './customer/components/product/Product.jsx'
+// // import ProductDetails from './customer/components/productDetails/ProductDetails.jsx'
+// // import CartContainer from './customer/components/cart/CartContainer.jsx'
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <>
+//       <Route path="/" element={<HorizontalLinearStepper />} />
+//       <Route path="/submit" action={action} element={<HomePage />} />
+//     </>
+//   )
+// )
+
+// function App() {
+//   return (
+//     <>
+//       <RouterProvider router={router} />
+//     </>
+//   )
+// }
+
+// export default App
--- Ecommerce-frontend/src/customer/components/homeSection/HomeSection.jsx
+++ Ecommerce-frontend/src/customer/components/homeSection/HomeSection.jsx
@@ -1,35 +1,31 @@
-import { useState } from "react"
-import women_dress from "../../data/women_dress"
+import { useState } from 'react'
+import { women_dress } from '../../data/women_dress'
 // the name of the "variable" after import is the name of the json file we are dragging the data from. this is when we are importing json data
-import women_jeans from "../../data/women_jeans"
-import men_shirt from "../../data/men_shirt"
-import women_top from "../../data/women_top"
-import men_jeans from "../../data/men_jeans"
-import women_gowns from "../../data/women_gowns"
-import women_attire from "../../data/women_attire"
-import men_pants from "../../data/men_pants"
-import men_shoes from "../../data/men_shoes"
-import MemorizedHomeSectionCarousel from "./MemorizedHomeSectionCarousel"
-
-
+import { women_jeans } from '../../data/women_jeans'
+import { men_shirt } from '../../data/men_shirt'
+import { women_top } from '../../data/women_top'
+import { men_jeans } from '../../data/men_jeans'
+import { women_gowns } from '../../data/women_gowns'
+import { women_attire } from '../../data/women_attire'
+import { men_pants } from '../../data/men_pants'
+import { men_shoes } from '../../data/men_shoes'
+import MemorizedHomeSectionCarousel from './MemorizedHomeSectionCarousel'
 
 const HomeSection = () => {
-
-
   // TODO: I beleive there must be changes to be doen here when it comes to whether the below variables/data must be states or not
-  
+
   // before dynamically fetching data from the backend using API:
-  const [men_shirt_state, ] = useState(men_shirt.slice(0,10))
+  const [men_shirt_state] = useState(men_shirt.slice(0, 10))
   // slice() method to create a new array with the first 10 elements of the original array
-  const [women_dress_state, ] = useState(women_dress.slice(0,10))
+  const [women_dress_state] = useState(women_dress.slice(0, 10))
   // we can directly use javascript methods on the imported json data
-  const [men_jeans_state, ] = useState(men_jeans.slice(0,10))
-  const [women_top_state, ] = useState(women_top.slice(0,10))
-  const [men_shoes_state, ] = useState(men_shoes.slice(0,10))
-  const [women_gowns_state, ] = useState(women_gowns.slice(0,10))
-  const [men_pants_state, ] = useState(men_pants.slice(0,10))
-  const [women_attire_state, ] = useState(women_attire.slice(0,10))
-  const [women_jeans_state, ] = useState(women_jeans.slice(0,10))
+  const [men_jeans_state] = useState(men_jeans.slice(0, 10))
+  const [women_top_state] = useState(women_top.slice(0, 10))
+  const [men_shoes_state] = useState(men_shoes.slice(0, 10))
+  const [women_gowns_state] = useState(women_gowns.slice(0, 10))
+  const [men_pants_state] = useState(men_pants.slice(0, 10))
+  const [women_attire_state] = useState(women_attire.slice(0, 10))
+  const [women_jeans_state] = useState(women_jeans.slice(0, 10))
 
   // any change in any of the above states will cause the re-rendering of all the child components below whether this changed state was the one passed for the child component or not. to prevent that we can use wrap the definition of the below child components with React.memo(), which will do a shallow comparison for the prop. if React.memo() shallow comparison could not prevent the re-rendering for certain case like re-rendering due to re-definition of a function defined in the parent component, then in this case to prevent that in case the re-rendering is expensive, you can use useMemo() or useCallback()
 
@@ -47,7 +43,6 @@ const HomeSection = () => {
     </section>
   )
 
-
   // // before dynamically fetching data from the backend using API:
   // const [men_shirt_state, setMen_shirt_state] = useState([])
   // // slice() method to create a new array with the first 10 elements of the original array
@@ -63,15 +58,13 @@ const HomeSection = () => {
 
   // // any change in any of the above states will cause the re-rendering of all the child components below whether this changed state was the one passed for the child component or not. to prevent that we can use wrap the definition of the below child components with React.memo(), which will do a shallow comparison for the prop. if React.memo() shallow comparison could not prevent the re-rendering for certain case like re-rendering due to re-definition of a function defined in the parent component, then in this case to prevent that in case the re-rendering is expensive, you can use useMemo() or useCallback()
 
-
   // const [isPending, startTransition] = useTransition();
-
 
   // useEffect(() => {
 
   //   // the usage of the setters must be inside useEffect() always to prevent infinite loop, because if the setters were added outside of the useEffect() or without it, the component will undergo initial rendering and then after the end of the initial rendering, the setters are executed and hence this will trigger another rendering and so on so forth.
   //   // state setters and the code inside useEffect are executed at the end of the current rendering and not during it, which means during the current rendering, they are not executed. after the end of the current render, they will be executed triggering the next re-renders
-    
+
   //   // note that startTransition() is typically used within handleFormChange for example where the user is interacting with the user interface, and before/above this startTransition() inside the handleFormChange() function will be the main functionality that must be served to the user when interacting with the interface
   //   // related to the above note: the code above startTransition() (and inside handleFormChange) has high-priority, and the code inside startTransition() has low-priority
   //   startTransition(() => {
@@ -101,10 +94,10 @@ const HomeSection = () => {
 
   // return (
   //   <section>
-  //     {isPending ? 
+  //     {isPending ?
   //     <p className="text-center text-3xl font-bold">
   //       Loading...
-  //     </p> : <MemorizedHomeSectionCarousel dataObject={men_shirt_state} />}          
+  //     </p> : <MemorizedHomeSectionCarousel dataObject={men_shirt_state} />}
   //         <MemorizedHomeSectionCarousel dataObject={women_dress_state} />
   //         <MemorizedHomeSectionCarousel dataObject={men_jeans_state} />
   //         <MemorizedHomeSectionCarousel dataObject={women_top_state} />
@@ -115,9 +108,6 @@ const HomeSection = () => {
   //         <MemorizedHomeSectionCarousel dataObject={women_jeans_state} />
   //   </section>
   // )
-
-
-
 }
 
 export default HomeSection
